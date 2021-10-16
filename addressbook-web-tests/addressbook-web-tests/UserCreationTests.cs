a5326cd55d000009378b6721687ac35cb9ef81f9
Alex Sizov <dr.sizov@gmail.com>
2021-10-16 13:10:35 +0300

    Move common code  to separate class. Add user creation tests.

---
+++ addressbook-web-tests/addressbook-web-tests/UserCreationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class UserCreationTests : CommonData
+    {
+        [Test]
+        public void UserCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GotoUsersPage();
+            UserData user = new UserData("Johnn", "Dow");
+            FillUserCreationForm(user);
+            Submit();
+            ReturnToHomepage();
+            logout();
+        }
+        
+        private void FillUserCreationForm(UserData user)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(user.Firstname);
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(user.Lastname);
+        }
+
+        private void GotoUsersPage()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }        
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/UserData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class UserData
+    {
+        private string firstname;
+        private string lastname;        
+
+        public UserData(string firstname)
+        {
+            this.firstname = firstname;
+        }
+        public UserData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;            
+        }
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }        
+    }
+}
